a3f4bdf6a7769a592a41b701323d5063d8db0f1c
Ari <arikaputkin@gmail.com>
2020-12-21 10:43:21 -0500

    added side-by-side

--- leaflet map.R
+++ leaflet map.R
@@ -1,22 +1,32 @@
 library(leaflet)
+library(leaflet.extras2)
 
-#BUILD MAP  
 leaflet() %>%
-  addProviderTiles(providers$CartoDB.PositronNoLabels) %>% 
+  addMapPane("left", zIndex = 0) %>%
+  addMapPane("right", zIndex = 0) %>%
   setView( -77.10542138315246, 42.749051268790645,  zoom = 13)%>%
-  addTiles("https://kaputkin.github.io/tileserver/tiles/NVDI/{z}/{x}/{y}.png", group = "NVDI")%>%  
-  hideGroup("NVDI")%>%  
-  addTiles("https://kaputkin.github.io/tileserver/tiles/grid/{z}/{x}/{y}.png", group = "Grid")%>%  
-  hideGroup("Grid")%>%
-  addTiles("https://kaputkin.github.io/tileserver/tiles/contours/{z}/{x}/{y}.png", group = "Contours")%>%  
-  hideGroup("Contours")%>%
-  addTiles("https://kaputkin.github.io/tileserver/tiles/halftone/{z}/{x}/{y}.png", group = "Halftone")%>%  
-
   
-#add legend
-addLayersControl(baseGroups = c("NVDI", "Grid", "Contours", "Halftone"),
-                 options = layersControlOptions(collapsed = FALSE))
-
-
-
-?layersControlOptions
+  
+  addProviderTiles(providers$CartoDB.PositronNoLabels, group="carto", layerId = "leftpane",
+                   options = pathOptions(pane = "left")) %>%
+  addProviderTiles(providers$CartoDB.PositronNoLabels, group="carto", layerId = "rightpane",
+                   options = pathOptions(pane = "right")) %>%
+  
+  
+  addTiles("https://kaputkin.github.io/tileserver/tiles/NVDI/{z}/{x}/{y}.png", group = "NVDI",
+           options = pathOptions(pane = "left")) %>%  
+  addTiles("https://kaputkin.github.io/tileserver/tiles/contours/{z}/{x}/{y}.png", group = "Contours",
+           options = pathOptions(pane = "right")) %>%
+  hideGroup("Contours")%>%
+  addTiles("https://kaputkin.github.io/tileserver/tiles/grid/{z}/{x}/{y}.png", group = "Hex Grid",
+           options = pathOptions(pane = "right")) %>%
+  hideGroup("Hex Grid")%>%
+  addTiles("https://kaputkin.github.io/tileserver/tiles/halftone/{z}/{x}/{y}.png", group = "Halftone",
+           options = pathOptions(pane = "right")) %>%
+  
+  
+  addLayersControl(baseGroups = c("Contours","Hex Grid", "Halftone"),
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  addSidebyside(layerId = "sidecontrols",
+                rightId = "rightpane",
+                leftId = "leftpane")
